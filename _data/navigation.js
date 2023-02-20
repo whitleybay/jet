48526f4e0506be7c786144a2c861f9ff0b015f0a
whitleybay <65926018+whitleybay@users.noreply.github.com>
2023-02-20 12:47:05 +0000

    Create navigation.js

---
+++ _data/navigation.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    "label": "First menu link",
+    "url": "/first-menu-link"
+  },
+  {
+    "label": "Second menu link",
+    "url": "/second-menu-link"
+  },
+  {
+    "label": "Third menu link",
+    "url": "/third-menu-link"
+  }
+]
